7278e545bdefeac4beec94b8659515c396594a46
PavanNarendraVasamsetti <narendrabunny0724@gmail.com>
2026-02-14 19:20:24 +0530

    fixing crash

--- app/qa.py
+++ app/qa.py
@@ -43,7 +43,9 @@ def build_vector_store():
     docs = splitter.create_documents(texts, metadatas=metadata)
 
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+          model_kwargs={"device":"cpu", "trust_remote_code": True},
+          encode_kwargs = {"normalize_embeddings": True, "batch_size" : 16}
     )
 
     db = FAISS.from_documents(docs, embeddings)
